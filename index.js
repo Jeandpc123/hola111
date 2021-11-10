ed3bb5810a83afe635f1701577306cbd8ea2ce3c
Jeandpc123 <91077401+Jeandpc123@users.noreply.github.com>
2021-11-10 12:48:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -52,11 +52,11 @@ const vcard = 'BEGIN:VCARD\n' // Tarjeta de contacto
             + 'VERSION:3.0\n' 
             + 'FN:Shan\n' // Nombre
             + 'ORG:Shanduy;\n' // Propietario
-            + 'TEL;type=CELL;type=VOICE;waid=5198891725:+593 96 768 9722\n' // ID de WhatsApp + número de teléfono
+            + 'TEL;type=CELL;type=VOICE;waid=593967689722:+593 96 768 9722\n' // ID de WhatsApp + número de teléfono
             + 'END:VCARD'
 /******FIN DE ENTRADA VCARD******/
 
-prefix = '.'
+prefix = ''
 blocked = []
 banChats = false
 
